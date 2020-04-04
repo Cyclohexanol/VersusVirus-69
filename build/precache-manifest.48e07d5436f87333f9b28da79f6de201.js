3253092c8a2672338dceb1c99392d4bb3299c7b9
Ambroise Mean <ambroise.mean@gmail.com>
2020-04-04 02:14:22 +0200

    build files

---
+++ build/precache-manifest.48e07d5436f87333f9b28da79f6de201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb98d0b156ca4edd80818999a24129c8",
+    "url": "/VersusVirus-69/index.html"
+  },
+  {
+    "revision": "1d6acbb8a07241ac8f0e",
+    "url": "/VersusVirus-69/static/css/main.574cb364.chunk.css"
+  },
+  {
+    "revision": "dc42d1f4cf81f793618d",
+    "url": "/VersusVirus-69/static/js/2.49593740.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VersusVirus-69/static/js/2.49593740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6acbb8a07241ac8f0e",
+    "url": "/VersusVirus-69/static/js/main.106810cb.chunk.js"
+  },
+  {
+    "revision": "7efa5b31eeb11472a3cd",
+    "url": "/VersusVirus-69/static/js/runtime-main.b9669c18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/VersusVirus-69/static/media/logo.5d5d9eef.svg"
+  }
+]);
