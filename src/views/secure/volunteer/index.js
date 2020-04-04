14c1621c6589e692d33638506224b7c81b3ae075
Ambroise Mean <ambroise.mean@gmail.com>
2020-04-04 16:18:15 +0200

    Cookie added !

--- src/views/secure/volunteer/index.js
+++ src/views/secure/volunteer/index.js
@@ -2,8 +2,33 @@ import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 class Volunteer extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      rangeKm: 10,
+      maxLoad: 15
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (key, value) {
+    this.setState(prevState => {
+      prevState[key] = value
+      return prevState
+    })
+  }
+
   render() {
-    return <div>Volunteer</div>;
+    return (
+      <section className="hero is-light">
+        <div className="hero-body">
+          <div className="container h-centered has-text-centered">
+            <button className="button is-large">Deliveries History</button>
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
