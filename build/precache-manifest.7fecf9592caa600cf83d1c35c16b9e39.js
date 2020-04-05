cc83b86a5edbe8cc46b048893255bf2809274c80
Jérémie Melly <melly.jeremie@gmail.com>
2020-04-05 15:48:17 +0200

    Test deploy without build

---
+++ build/precache-manifest.7fecf9592caa600cf83d1c35c16b9e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f858b3838394661e089629de0270a95d",
+    "url": "/VersusVirus-69/index.html"
+  },
+  {
+    "revision": "e5ac7e139fbeee19ebcc",
+    "url": "/VersusVirus-69/static/css/main.b3bb63cc.chunk.css"
+  },
+  {
+    "revision": "eb39c8c5161e2f9dd250",
+    "url": "/VersusVirus-69/static/js/2.cf2a5927.chunk.js"
+  },
+  {
+    "revision": "2bd07c3cd0f9b6661e94615603614d5a",
+    "url": "/VersusVirus-69/static/js/2.cf2a5927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5ac7e139fbeee19ebcc",
+    "url": "/VersusVirus-69/static/js/main.0b738b65.chunk.js"
+  },
+  {
+    "revision": "6d5b690260dde95faf8d",
+    "url": "/VersusVirus-69/static/js/runtime-main.7b195dd1.js"
+  }
+]);
