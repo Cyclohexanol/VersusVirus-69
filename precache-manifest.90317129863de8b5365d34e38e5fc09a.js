3277f981a0534ea6cc3061b06a2081cd065a7e17
Jérémie Melly <melly.jeremie@gmail.com>
2020-04-05 19:08:58 +0200

    Updates

---
+++ precache-manifest.90317129863de8b5365d34e38e5fc09a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99be3e3d15ea0ad6e49a6eaabd849f4d",
+    "url": "/VersusVirus-69/index.html"
+  },
+  {
+    "revision": "a4e8ffb87a87195f19b9",
+    "url": "/VersusVirus-69/static/css/main.1b7e425c.chunk.css"
+  },
+  {
+    "revision": "db843e98520902ce0d91",
+    "url": "/VersusVirus-69/static/js/2.a58e768a.chunk.js"
+  },
+  {
+    "revision": "2bd07c3cd0f9b6661e94615603614d5a",
+    "url": "/VersusVirus-69/static/js/2.a58e768a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4e8ffb87a87195f19b9",
+    "url": "/VersusVirus-69/static/js/main.a132f28f.chunk.js"
+  },
+  {
+    "revision": "0b78fc67af3258e4a31a",
+    "url": "/VersusVirus-69/static/js/runtime-main.1b98a28e.js"
+  }
+]);
