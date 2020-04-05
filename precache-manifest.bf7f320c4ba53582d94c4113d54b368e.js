429ecada1ea6e958d3e87105b6b0f9e5819869b9
Jérémie Melly <melly.jeremie@gmail.com>
2020-04-05 15:28:13 +0200

    Updates

---
+++ precache-manifest.bf7f320c4ba53582d94c4113d54b368e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6889d322cdde8b46e90a4edde463303d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "106f0a59f545acc124ec",
+    "url": "/static/css/main.b3bb63cc.chunk.css"
+  },
+  {
+    "revision": "eb39c8c5161e2f9dd250",
+    "url": "/static/js/2.cf2a5927.chunk.js"
+  },
+  {
+    "revision": "2bd07c3cd0f9b6661e94615603614d5a",
+    "url": "/static/js/2.cf2a5927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106f0a59f545acc124ec",
+    "url": "/static/js/main.1c910d13.chunk.js"
+  },
+  {
+    "revision": "9509ff57ba4029690c5a",
+    "url": "/static/js/runtime-main.404b696b.js"
+  }
+]);
